83a2b9ddb16ab993c99c7fd2f368b7eea72383b6
Kyu Lee <kwooler@gmail.com>
2023-01-19 10:40:21 -0800

    A42

--- main_test.py
+++ main_test.py
@@ -7,7 +7,7 @@ import re
 def test_main_1():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    datastr = 'a\nf'
+    datastr = 'Hello\nThere\ndone'
     sys.stdin = io.StringIO(datastr)
 
     main.main()
@@ -18,15 +18,17 @@ def test_main_1():
 
     # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
     # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*a'
-    regex_string += r'[\w,\W]*b'
-    regex_string += r'[\w,\W]*c'
-    regex_string += r'[\w,\W]*d'
-    regex_string += r'[\w,\W]*e'
-    regex_string += r'[\w,\W]*f'
+    regex_string = r'[\w,\W]*olleH'
     regex_string += r'[\w,\W]*'
     print(regex_string)
     res = re.search(regex_string, lines[0])
+    assert res != None
+    print(res.group())
+
+    regex_string = r'[\w,\W]*erehT'
+    regex_string += r'[\w,\W]*'
+    print(regex_string)
+    res = re.search(regex_string, lines[1])
     assert res != None
     print(res.group())
 
@@ -34,7 +36,7 @@ def test_main_1():
 def test_main_2():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    datastr = 'f\na\na\nf'
+    datastr = 'ABCDEF\ndone'
     sys.stdin = io.StringIO(datastr)
 
     main.main()
@@ -45,39 +47,9 @@ def test_main_2():
 
     # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
     # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*a'
-    regex_string += r'[\w,\W]*b'
-    regex_string += r'[\w,\W]*c'
-    regex_string += r'[\w,\W]*d'
-    regex_string += r'[\w,\W]*e'
-    regex_string += r'[\w,\W]*f'
+    regex_string = r'[\w,\W]*FEDCBA'
     regex_string += r'[\w,\W]*'
     print(regex_string)
-    res = re.search(regex_string, lines[1])
-    assert res != None
-    print(res.group())
-
-
-def test_main_3():
-    captureOut = io.StringIO()
-    sys.stdout = captureOut
-    datastr = '1\n2\no\ns'
-    sys.stdin = io.StringIO(datastr)
-
-    main.main()
-    sys.stdout = sys.__stdout__
-    print('Captured ', captureOut.getvalue())
-    lines = captureOut.getvalue().split('\n')
-    print(lines)
-
-    # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
-    # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*o'
-    regex_string += r'[\w,\W]*p'
-    regex_string += r'[\w,\W]*q'
-    regex_string += r'[\w,\W]*r'
-    regex_string += r'[\w,\W]*s'
-    regex_string += r'[\w,\W]*'
-    res = re.search(regex_string, lines[1])
+    res = re.search(regex_string, lines[0])
     assert res != None
     print(res.group())
