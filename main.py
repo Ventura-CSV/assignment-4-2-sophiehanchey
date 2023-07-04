7a7511ed86f4acc834207987804b8ec20b055546
Kyuwoong Lee <kwooler@gmail.com>
2023-07-04 13:04:41 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 def main():
-    ##################################################
-    # Comlete your code here
-    ##################################################
+    result = []
+    a1 = int(input('Enter your first number: '))
+    a2 = int(input('Enter your second number: '))
+    N = int(input('Enter the number of sequences: '))
+    result.append(a1)
+    result.append(a2)
+    """
+    ########################################
+    Code Your Program here
+    ########################################
+    """
 
-    pass
+    ########################################
+    # Do not delete the return statement
+    ########################################
+    return result
+
 
 
 if __name__ == '__main__':
